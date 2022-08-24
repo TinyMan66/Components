98f48992b283ecf4ea627902b8653e6478ba8fe2
Diana Lipanova <altere2804@gmail.com>
2022-08-24 17:12:08 +0300

    Tested React.memo as useCallback

--- src/stories/UseMemo.stories.tsx
+++ src/stories/UseMemo.stories.tsx
@@ -1,4 +1,5 @@
 import React, {useMemo, useState} from "react";
+import {Select} from "../Components/Select/Select";
 
 export default {
     title: 'useMemo',
@@ -71,3 +72,85 @@ export const HelpToReactMemo = () => {
         <Users users={newArray}/>
     </>
 }
+
+
+//task 18lesson (not finished)
+const cities = [
+    {countryID: 1, population: 20, title: 'Moscow'},
+    {countryID: 1, population: 5, title: 'Kaliningrad'},
+    {countryID: 1, population: 15, title: 'St-Petersburg'},
+    {countryID: 2, population: 10, title: 'Stockholm'},
+    {countryID: 2, population: 5, title: 'Vetlanda'},
+    {countryID: 2, population: 4, title: 'Sundviken'},
+    {countryID: 3, population: 12, title: 'Lisbon'},
+    {countryID: 3, population: 14, title: 'Porto'},
+    {countryID: 3, population: 7, title: 'Lagos'},
+]
+
+const SelectMemo = React.memo(Select);
+
+export const HelpExample = () => {
+    console.log('HelpExample')
+    const [number, setNumber] = useState<number>(0);
+
+    // const citiesWithLetterM = cities.filter(c => c.title.toLowerCase().indexOf('m') > -1);
+    // const citiesRussian = cities.filter(c => c.countryID === 1);
+    // const citiesPopulation = cities.filter(c => c.population > 10);
+    const citiesWithLetterM = useMemo(() => {
+        return cities.filter(c => c.title.toLowerCase().indexOf('l') === 0)
+    }, [cities]);
+
+    const citiesRussian = useMemo(() => {
+        return cities.filter(c => c.countryID === 1)
+    }, [cities])
+
+    const citiesPopulation = useMemo(() => {
+        return cities.filter(c => c.population > 10)
+    }, [cities])
+
+
+    return <>
+        <div>
+            <button onClick={() => {setNumber(number + 1)}}>+</button>
+            {number}
+        </div>
+        <SelectMemo onChange={() => {}} items={citiesWithLetterM}/>
+        <SelectMemo onChange={() => {}} items={citiesRussian}/>
+        <SelectMemo onChange={() => {}} items={citiesPopulation}/>
+    </>
+}
+
+export const LikeUseCallback = () => {
+    console.log('LikeUseCallback');
+    const [counter, setCounter] = useState(0);
+    const [books, setBooks] = useState(['React', 'JS', 'CSS','HTML']);
+
+    const addBook = () => {
+        console.log(books)
+        const newBooks = [...books, 'Angular' + new Date().getTime()];
+        setBooks(newBooks);
+    }
+
+    const memoizedAddBook = useMemo(() => {
+        return addBook
+    }, [books]);
+
+    return <>
+        <button onClick={() => setCounter(counter + 1)}>+</button>
+        {counter}
+        <Book addBook={memoizedAddBook}/>
+    </>
+}
+
+type BooksSecretPropsType = {
+    addBook: () => void
+}
+
+const BooksSecret = (props: BooksSecretPropsType) => {
+    console.log('BooksSecret')
+    return <div>
+        <button onClick={() => props.addBook()}>add book</button>
+    </div>
+}
+
+const Book = React.memo(BooksSecret);
