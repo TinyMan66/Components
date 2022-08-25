9721101dcc7a1a2a72341a601fdb0e57fa0653d5
Diana Lipanova <altere2804@gmail.com>
2022-08-25 16:04:42 +0300

    Create UseEffect.stories.tsx. Test different dependencies in useEffect.

---
+++ src/stories/UseEffect.stories.tsx
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from "react";
+
+export default {
+    title: 'useEffect demo',
+}
+
+export const SimpleExample = () => {
+    const [fake, setFake] = useState(1);
+    const [counter, setCounter] = useState(1);
+
+    console.log("SimpleExample");
+
+    useEffect(() => {
+        console.log("useEffect every render"); //no dependencies
+        document.title = counter.toString();
+    });
+
+    useEffect(() => {
+        console.log("useEffect only first render (like componentDidMount)"); //empty dependency array
+        document.title = counter.toString();
+    }, []);
+
+    useEffect(() => {
+        console.log("useEffect first render and every counter change"); //counter array dependency
+        document.title = counter.toString();
+    }, [counter]);
+
+
+    return <>
+        Hello, {counter} {fake}
+        <button onClick={() => setFake(fake + 1)}>Fake+</button>
+        <button onClick={() => setCounter(counter + 1)}>Counter+</button>
+    </>
+}
