f7ba3406778d1bac2a3dbb6130bcb6c162fa4e0c
Diana Lipanova <altere2804@gmail.com>
2022-08-25 15:03:13 +0300

    Using useMemo for remembering initial value

--- src/stories/UseState.stories.tsx
+++ src/stories/UseState.stories.tsx
@@ -1,12 +1,20 @@
-import React, {useState} from "react";
+import React, {useMemo, useState} from "react";
 
 export default {
     title: 'useState demo',
 }
+function generateData(){
+    //difficult counting
+    console.log("generateData")
+    return 37654339399;
+}
 
 export const Example1 = () => {
     console.log("Example1");
-    const [counter, setCounter] = useState(0);
+
+    const initValue = useMemo(generateData, []);
+
+    const [counter, setCounter] = useState(initValue);
 
     return <>
         <button onClick={() => setCounter(counter + 1)}>+</button>
