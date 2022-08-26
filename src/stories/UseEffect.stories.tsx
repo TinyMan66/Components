4a787e2e52f80339fc44d9a5a7373d19ac955924
Diana Lipanova <altere2804@gmail.com>
2022-08-26 12:29:33 +0300

    Created timer used setInterval and useEffect

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -69,3 +69,25 @@ export const SetIntervalExample = () => {
         Hello, counter: {counter}
     </>
 }
+
+export const ClockExample = () => {
+
+    const [seconds, setSeconds] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [hours, setHours] = useState(0);
+
+    console.log("ClockExample");
+
+    useEffect(() => {
+        setInterval(() => {
+            let date = new Date();
+            setHours(date.getHours())
+            setMinutes(date.getMinutes())
+            setSeconds(date.getSeconds())
+        }, 1000);
+    }, [] );
+
+    return <>
+        Hello, time: {hours}:{minutes}:{seconds}
+    </>
+}
