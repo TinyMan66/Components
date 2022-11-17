23579c76f6acc746f4939d4797dc19f4ebf698bf
Diana Lipanova <altere2804@gmail.com>
2022-11-17 12:42:31 +0100

    Added AnalogClock component

--- src/Components/Clock/Clock.stories.tsx
+++ src/Components/Clock/Clock.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Clock} from "./Clock";
 
 
@@ -6,6 +6,28 @@ export default {
     title: 'Clock',
     component: Clock,
 };
+
+export const ClockExample = () => {
+
+    const [seconds, setSeconds] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [hours, setHours] = useState(0);
+
+    console.log("ClockExample");
+
+    useEffect(() => {
+        setInterval(() => {
+            let date = new Date();
+            setHours(date.getHours())
+            setMinutes(date.getMinutes())
+            setSeconds(date.getSeconds())
+        }, 1000);
+    }, [] );
+
+    return <>
+        Hello, time: {hours}:{minutes}:{seconds}
+    </>
+}
 
 export const BaseAnalogExample = () => {
     return <Clock mode={'analog'}/>;
