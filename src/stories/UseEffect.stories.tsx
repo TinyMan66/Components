dd7766138f4e072a7cd88b2553a38d4318f403b4
Diana Lipanova <altere2804@gmail.com>
2022-11-17 14:04:40 +0100

    Added KeysTrackerExample stories component

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -63,29 +63,56 @@ export const SetIntervalExample = () => {
         setInterval(() => {
             setCounter(state => state + 1)
         }, 1000);
-    }, [] );
+    }, []);
 
     return <>
         Hello, counter: {counter}
     </>
 }
+
 export const ResetEffectExample = () => {
     const [counter, setCounter] = useState(0);
 
     console.log("Component rendered with " + counter);
 
-    const increase = () => {setCounter(counter + 1)};
+    const increase = () => {
+        setCounter(counter + 1)
+    };
     useEffect(() => {
         console.log("Effect occurred " + counter)
 
         return () => {
             console.log("RESET EFFECT " + counter)
         }
-    }, [counter] );
+    }, [counter]);
 
 
     return <>
-        Hello, counter: {counter} <button onClick={increase}>+</button>
+        Hello, counter: {counter}
+        <button onClick={increase}>+</button>
+    </>
+}
+
+export const KeysTrackerExample = () => {
+    const [text, setText] = useState("");
+
+    console.log("Component rendered with " + text);
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            console.log(e.key)
+            setText((state) => state + e.key)
+        };
+
+        window.addEventListener('keypress', handler)
+        return () => {
+            window.removeEventListener('keypress', handler)
+        }
+    }, []);
+
+
+    return <>
+        Typed text: {text}
     </>
 }
 
